6cc0f70005d038cc8650003d06c187ebd045536c
koral <orszulak.k@o2.pl>
2020-12-03 11:08:42 +0100

    voting system

--- src/main/java/koral/jbwmmiscellaneous/Voting.java
+++ src/main/java/koral/jbwmmiscellaneous/Voting.java
@@ -1,6 +1,4 @@
 package koral.jbwmmiscellaneous;
-import net.md_5.bungee.api.ChatMessageType;
-import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.World;
