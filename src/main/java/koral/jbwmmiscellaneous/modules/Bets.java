5bd20b44deb0336b6abc3615e8e6398f2db79dff
koral <orszulak.k@o2.pl>
2020-12-12 04:23:53 +0100

    Bets, BlackJack

--- src/main/java/koral/jbwmmiscellaneous/modules/Bets.java
+++ src/main/java/koral/jbwmmiscellaneous/modules/Bets.java
@@ -4,7 +4,6 @@ import koral.jbwmmiscellaneous.managers.CommandManager;
 import koral.jbwmmiscellaneous.managers.ConfigManager;
 import koral.jbwmmiscellaneous.managers.ModuleManager;
 import net.md_5.bungee.api.ChatColor;
-import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -12,17 +11,15 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
-
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.UUID;
 
 @ModuleManager.Moduł
 public class Bets extends CommandManager implements Listener {
     ConfigManager betsConfig = new ConfigManager("bets.yml");
     HashMap<String, Integer> bets = new HashMap<>();
+
     public Bets() {
         super("bet");
     }
@@ -35,7 +32,7 @@ public class Bets extends CommandManager implements Listener {
     public ItemStack frakcjowaluta() {
         ItemStack itemStack = new ItemStack(Material.GOLD_NUGGET);
         ItemMeta itemMeta = itemStack.getItemMeta();
-        itemMeta.setDisplayName("§eZlotaMoneta");
+        itemMeta.setDisplayName("§e§lZlotaMoneta");
         ArrayList<String> lore = new ArrayList<>();
         lore.add("§8Frakcjonawkowa Waluta");
         itemMeta.setLore(lore);
@@ -46,7 +43,7 @@ public class Bets extends CommandManager implements Listener {
     public int getValue(Player player) {
         int toDelete = 0;
         for (ItemStack itemStack : player.getInventory().getContents()) {
-            if (itemStack != null && itemStack.getItemMeta().getDisplayName().equals("§eZlotaMoneta")) {
+            if (itemStack != null && itemStack.getItemMeta().getDisplayName().equals("§e§lZlotaMoneta")) {
                 toDelete = itemStack.getAmount() + toDelete;
             }
         }
@@ -64,53 +61,101 @@ public class Bets extends CommandManager implements Listener {
         }
     }
 
+
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         if (sender instanceof Player) {
-            if (cmd.getName().equals("bet") && args.length == 1 && isInt(args[0])) {
-                Player player = (Player) sender;
-                int total = getValue(player);
-                if (total >= Integer.valueOf(args[0])) {
-                    for (int i = 0; i < Integer.valueOf(args[0]); i++)
-                        player.getInventory().removeItemAnySlot(frakcjowaluta());
-                    player.sendMessage(ChatColor.GREEN + "Obstawiłeś " + Integer.valueOf(args[0]) + " na gracza ");
-                    int previous;
-                    if (bets.containsKey(player.getUniqueId().toString())) {
-                        previous = bets.get(player.getUniqueId().toString());
-                        bets.put(player.getUniqueId().toString(), previous + Integer.valueOf(args[0]));
-
-                    } else
-                        bets.put(player.getUniqueId().toString(), Integer.valueOf(args[0]));
-                } else {
-                    player.sendMessage(ChatColor.RED + "Nie masz przy sobie wystarczająco waluty, aby obstawić!");
-                    return true;
-                }
-            }
-
-
-
-                switch (args[0]) {
-                    case "fighter":
-                        if(args.length == 2){
-                            betsConfig.config.set("Fighter", "test");
+            Player player = (Player) sender;
+            if (cmd.getName().equals("bet") && args.length == 2 && isInt(args[1])) {
+                if (betsConfig.getConfig().contains(args[0])) {
+                    int total = getValue(player);
+                    if (total >= Integer.valueOf(args[1])) {
+                        for (int i = 0; i < Integer.valueOf(args[1]); i++)
+                            player.getInventory().removeItemAnySlot(frakcjowaluta());
+                        if (betsConfig.getConfig().contains(args[0] + "." + player.getName())) {
+                            int value = betsConfig.getConfig().getInt(args[0] + "." + sender.getName());
+                            betsConfig.getConfig().set(args[0] + "." + player.getName(), value + Integer.valueOf(args[1]));
                             betsConfig.save();
-                            sender.sendMessage("Zapisano fightera");
+                        } else {
+                            betsConfig.config.set(args[0] + "." + sender.getName(), Integer.valueOf(args[1]));
+                            betsConfig.save();
                         }
-                        break;
-                    case "reload":
-                        break;
-                    case "info":
-                        break;
-                    case "winners":
-                        break;
-                    case "zaklady":
-                        bets.entrySet().forEach(entry -> {
-                            String name = Bukkit.getServer().getOfflinePlayer(UUID.fromString(entry.getKey())).getName();
-                            sender.sendMessage("§7Obstawiający: §a" + name + "§7 ilosc: §e" + entry.getValue());
-                        });
+                        player.sendMessage(ChatColor.GREEN + "Obstawiłeś " + Integer.valueOf(args[1]) + " na gracza " + args[0]);
+                    } else {
+                        player.sendMessage(ChatColor.RED + "Nie masz przy sobie wystarczająco waluty, aby obstawić!");
+                        return true;
+                    }
+                } else {
+                    sender.sendMessage("Taka osoba nie jest dostepna do glosowania!");
                 }
             }
+
+            switch (args[0]) {
+                case "dodaj":
+                    if (args.length == 2) {
+                        betsConfig.config.set(args[1], "");
+                        betsConfig.save();
+                        sender.sendMessage("Zapisano bijącego się");
+                    }
+                    break;
+                case "usun": {
+                    if (args.length == 2)
+                        betsConfig.config.set(args[1], null);
+                    betsConfig.save();
+                    sender.sendMessage("usunięto zawodnika");
+                }
+                break;
+                case "reload":
+                    break;
+                case "info":
+                    wyswietlInfo(player);
+                    break;
+                case "winners":
+                    break;
+                case "zaklady":
+                    //            bets.entrySet().forEach(entry -> {
+                    //              String name = Bukkit.getServer().getOfflinePlayer(UUID.fromString(entry.getKey())).getName();
+                    //            sender.sendMessage("§7Obstawiający: §a" + name + "§7 ilosc: §e" + entry.getValue());
+                    //      });
+                    break;
+                case "zamknijzaklady":
+                    break;
+
+            }
+        }
 
         return true;
     }
-}
+    ArrayList<String> osoby = new ArrayList<>();
+    double kursTarget1;
+    double kursTarget2;
+
+    public void wyswietlInfo(Player player) {
+        player.sendMessage("§7Dostępni zawodnicy do obstawienia: ");
+        liczKurs();
+        for (String keys : betsConfig.getConfig().getKeys(false)) {
+            if(keys.equals(osoby.get(0)))
+            player.sendMessage("§71." + keys + " §4Kurs: " + kursTarget1);
+            if(keys.equals(osoby.get(1)))
+                player.sendMessage("§72." + keys + " §4Kurs: " + kursTarget2);
+        }
+    }
+    public void liczKurs() {
+        double total1target = 0;
+        double total2target = 0;
+        for(String key : betsConfig.getConfig().getKeys(false)){
+            osoby.add(key);
+        }
+        for(String key : betsConfig.getConfig().getConfigurationSection(osoby.get(0) + ".").getKeys(false)){
+           total1target = total1target + betsConfig.getConfig().getInt(osoby.get(0) + "." + key);
+        }
+        for(String key : betsConfig.getConfig().getConfigurationSection(osoby.get(1) + ".").getKeys(false)){
+            total2target = total2target + betsConfig.getConfig().getInt(osoby.get(1) + "." + key);
+        }
+
+        //100 do 250
+        // 100/250 == 0,4
+        //1,67 - 0,67 * %
+        //1,67 + 1,67 *
+     }
+    }
---
+++ src/main/java/koral/jbwmmiscellaneous/modules/BlackJack.java
@@ -0,0 +1,65 @@
+package koral.jbwmmiscellaneous.modules;
+
+import koral.jbwmmiscellaneous.managers.CommandManager;
+import koral.jbwmmiscellaneous.managers.ModuleManager;
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+@ModuleManager.Moduł
+public class BlackJack extends CommandManager implements Listener {
+    Map<String, Long> cd = new HashMap<>();
+
+    public BlackJack() {
+        super("bj");
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command cmd, String label, String[] args) {
+        return null;
+    }
+
+    int karty = 52;
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        int x = 1; //min
+        int y = 11; // max
+        Player player = (Player) sender;
+        if (!cd.containsKey(player.getUniqueId().toString()) || cd.get(player.getUniqueId().toString()) < System.currentTimeMillis() / 1000) {
+            Random random = new Random();
+            int losowa = random.nextInt(y - x + 1) + x;
+            if (losowa != 1) {
+                sendToNearby(player, "§8§l[BLACKJACK] §f" + player.getDisplayName() + "§8 wylosował/a §e" + losowa);
+            } else {
+                sendToNearby(player, ("§8§l[BLACKJACK] §f" + player.getDisplayName() + "§8 wylosował/a §eASA"));
+            }
+            cd.put(player.getUniqueId().toString(), System.currentTimeMillis() / 1000 + 2);
+        } else
+            player.sendMessage("§4Musisz jeszcze odczekać chwilę, aby tego użyć.");
+
+        return true;
+    }
+
+    int distance = 20;
+
+    private void sendToNearby(Player player, String message) {
+        for (Entity entity : player.getLocation().getNearbyEntities(16, 16, 16)) {
+            if (entity instanceof Player) {
+                Player p = (Player) entity;
+                p.sendMessage(message);
+            }
+        }
+
+    }
+}
